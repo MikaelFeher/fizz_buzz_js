ef8d5f23c7ab63349832856f457a5c668e069a2d
MikaelFeher <mikael.feher@gmail.com>
2016-06-25 22:15:16 +0200

    added tests for the fizzbuzz result

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -8,5 +8,8 @@ describe("fizz_buzz", function() {
   });
   it("returns 'buzz' if number is divisible by 5", function() {
     expect(fizz_buzz(5)).toEqual('Buzz');
+  });
+  it("returns 'fizzbuzz' if number is divisible by 15", function(){
+    expect(fizz_buzz(15)).toEqual('FizzBuzz');
   })
 });
